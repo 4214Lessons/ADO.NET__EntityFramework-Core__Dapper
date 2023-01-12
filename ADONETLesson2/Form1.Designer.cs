110a04b812b4dc4056026764468103f12846a06c
Tural Novruzov <turalinovruzov@gmail.com>
2023-01-12 22:16:42 +0400

    Disconnected Mode

---
+++ ADONETLesson2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ADONETLesson2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.Btn_Exec = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Btn_Fill = new System.Windows.Forms.Button();
+            this.Btn_Update = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtCommand.Location = new System.Drawing.Point(12, 12);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(1099, 43);
+            this.txtCommand.TabIndex = 0;
+            // 
+            // Btn_Exec
+            // 
+            this.Btn_Exec.AutoSize = true;
+            this.Btn_Exec.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btn_Exec.Location = new System.Drawing.Point(12, 61);
+            this.Btn_Exec.Name = "Btn_Exec";
+            this.Btn_Exec.Size = new System.Drawing.Size(124, 48);
+            this.Btn_Exec.TabIndex = 1;
+            this.Btn_Exec.Text = "EXEC";
+            this.Btn_Exec.UseVisualStyleBackColor = true;
+            this.Btn_Exec.Click += new System.EventHandler(this.Btn_Exec_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(1099, 508);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Btn_Fill
+            // 
+            this.Btn_Fill.AutoSize = true;
+            this.Btn_Fill.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btn_Fill.Location = new System.Drawing.Point(857, 61);
+            this.Btn_Fill.Name = "Btn_Fill";
+            this.Btn_Fill.Size = new System.Drawing.Size(124, 48);
+            this.Btn_Fill.TabIndex = 1;
+            this.Btn_Fill.Text = "Fill";
+            this.Btn_Fill.UseVisualStyleBackColor = true;
+            this.Btn_Fill.Click += new System.EventHandler(this.Btn_Fill_Click);
+            // 
+            // Btn_Update
+            // 
+            this.Btn_Update.AutoSize = true;
+            this.Btn_Update.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btn_Update.Location = new System.Drawing.Point(987, 61);
+            this.Btn_Update.Name = "Btn_Update";
+            this.Btn_Update.Size = new System.Drawing.Size(124, 48);
+            this.Btn_Update.TabIndex = 1;
+            this.Btn_Update.Text = "Update";
+            this.Btn_Update.UseVisualStyleBackColor = true;
+            this.Btn_Update.Click += new System.EventHandler(this.Btn_Update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 635);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Btn_Update);
+            this.Controls.Add(this.Btn_Fill);
+            this.Controls.Add(this.Btn_Exec);
+            this.Controls.Add(this.txtCommand);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtCommand;
+        private Button Btn_Exec;
+        private DataGridView dataGridView1;
+        private Button Btn_Fill;
+        private Button Btn_Update;
+    }
+}
---
+++ ADONETLesson2/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace ADONETLesson2;
+
+
+
+public partial class Form1 : Form
+{
+    SqlConnection? conn = null;
+    SqlDataReader? reader = null;
+    DataTable? table = null;
+
+    SqlDataAdapter? dataAdapter = null;
+    DataSet? dataSet = null;
+    SqlCommandBuilder? cmdBuilder = null;
+
+
+
+    public Form1()
+    {
+        InitializeComponent();
+        conn = new("Data Source=(localdb)\\ProjectModels;Initial Catalog=Library;Integrated Security=True;");
+    }
+
+
+
+    private void WorkingWithDataTable()
+    {
+        DataTable table = new DataTable();
+
+
+        // DataColumnCollection
+        // DataRowCollection
+
+
+
+        //DataColumn column = new DataColumn()
+        //{
+        //    AllowDBNull = false,
+        //    DataType = typeof(int),
+        //    DefaultValue = 0,
+        //    ColumnName = "Id"
+        //};
+
+
+
+        table.Columns.Add("Id");
+        table.Columns.Add("FirstName");
+        table.Columns.Add("LastName");
+
+
+        // DataRow row = table.NewRow();
+
+        table.Rows.Add(1, "Isa", "Məmmədli");
+        table.Rows.Add(2, "Ali", "Şamilzadə");
+
+
+
+        dataGridView1.DataSource = table;
+    }
+
+
+
+
+    // DataTable with Connected Mode
+    private void Btn_Exec_Click(object sender, EventArgs e)
+    {
+        // WorkingWithDataTable();
+
+
+        try
+        {
+            using var command = new SqlCommand(txtCommand.Text, conn);
+
+            conn?.Open();
+
+
+            table = new DataTable();
+            reader = command.ExecuteReader();
+
+
+            bool isColumnName = true;
+
+            do
+            {
+                while (reader.Read())
+                {
+
+                    if (isColumnName)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            table.Columns.Add(reader.GetName(i));
+
+                        isColumnName = false;
+                    }
+
+
+                    DataRow row = table.NewRow();
+
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        row[i] = reader[i];
+
+                    table.Rows.Add(row);
+                }
+            } while (reader.NextResult());
+
+
+            dataGridView1.DataSource = table;
+        }
+
+        catch
+        {
+            MessageBox.Show("Probably wrong request syntax");
+        }
+        finally
+        {
+            // Close the connection
+            conn?.Close();
+            reader?.Close();
+        }
+    }
+
+
+
+
+    // Disconnected Mode
+    // 1) DataSet
+    // 2) DbDataAdapter
+    private void Btn_Fill_Click(object sender, EventArgs e)
+    {
+        // DisconnectedMode();
+        // ExampleSqlCommandBuilder();
+        CustomUpdateCommand();
+    }
+
+
+
+    private void DisconnectedMode()
+    {
+        dataAdapter = new SqlDataAdapter("SELECT * FROM Books; SELECT * FROM Authors", conn);
+
+
+        //// Open()
+        //// SqlCommand
+        //// SqlDataReader
+        //// Close()
+
+
+        // table = new DataTable();
+        dataSet = new DataSet();
+
+
+        //// SqlDataAdapter
+        ////      Fill()
+        ////      Update()
+
+        dataAdapter.Fill(dataSet, "myTable");
+
+
+
+        // dataGridView1.DataSource = dataSet.Tables[0];
+        // dataGridView1.DataSource = dataSet.Tables[1];
+        // dataGridView1.DataSource = dataSet.Tables["table"];
+        //dataGridView1.DataSource = dataSet.Tables["table1"];
+        dataGridView1.DataSource = dataSet.Tables["myTable1"];
+
+
+        // MessageBox.Show(dataSet.Tables["Books1"]?.Rows[17][1].ToString());
+
+
+
+        //// DataSet vs SqlDataReader
+    }
+
+
+
+
+
+
+    private void ExampleSqlCommandBuilder()
+    {
+        string selectSQL = "SELECT * FROM Authors;";
+
+        dataAdapter = new SqlDataAdapter(selectSQL, conn);
+
+
+        cmdBuilder = new SqlCommandBuilder(dataAdapter);
+        //// cmdBuilder.RefreshSchema();
+
+        dataSet = new DataSet();
+        dataAdapter.Fill(dataSet, "myTable");
+        // adapter.Fill(dataSet, 2, 10, "myTable");
+
+
+        dataGridView1.DataSource = dataSet.Tables[0];
+
+
+        Debug.WriteLine(cmdBuilder.GetInsertCommand().CommandText);
+        Debug.WriteLine(cmdBuilder.GetUpdateCommand().CommandText);
+        Debug.WriteLine(cmdBuilder.GetDeleteCommand().CommandText);
+    }
+
+
+
+    private void Btn_Update_Click(object sender, EventArgs e)
+    {
+        if (dataSet is not null)
+            dataAdapter?.Update(dataSet, "myTable");
+    }
+
+
+
+
+
+    private void CustomUpdateCommand()
+    {
+        string selectSQL = "SELECT * FROM Books";
+        dataAdapter = new SqlDataAdapter(selectSQL, conn);
+
+
+        dataSet = new DataSet();
+        dataAdapter.Fill(dataSet, "myTable");
+
+
+
+        //// Way 1
+        // SqlCommand updateCommand = new SqlCommand("UPDATE Books SET Pages=@pPages WHERE Id=@pId", conn);
+
+
+
+        // Way 2
+        SqlCommand updateCommand = new SqlCommand()
+        {
+            CommandText = "usp_UpdateBooks",
+            Connection = conn,
+            CommandType = CommandType.StoredProcedure,
+        };
+
+
+
+
+        updateCommand.Parameters.Add(new SqlParameter("@pId", SqlDbType.Int));
+        updateCommand.Parameters["@pId"].SourceVersion = DataRowVersion.Original;
+        updateCommand.Parameters["@pId"].SourceColumn = "Id";
+
+
+        updateCommand.Parameters.Add(new SqlParameter("@pPages", SqlDbType.Int));
+        updateCommand.Parameters["@pPages"].SourceVersion = DataRowVersion.Current;
+        updateCommand.Parameters["@pPages"].SourceColumn = "Pages";
+
+
+        dataAdapter.UpdateCommand = updateCommand;
+
+
+        dataGridView1.DataSource = dataSet.Tables[0];
+
+        MessageBox.Show(dataAdapter.UpdateCommand.CommandText);
+    }
+}
