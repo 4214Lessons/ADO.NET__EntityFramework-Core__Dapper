a16e384a861b26b21cc462a6d5e4ac14e7227473
Tural Novruzov <turalinovruzov@gmail.com>
2023-01-31 19:22:02 +0400

    Add Lesson 8

---
+++ Lesson8_EntityFrameworkCore_CodeFirst/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Lesson8_EntityFrameworkCore_CodeFirst.Models;
+
+namespace Lesson8_EntityFrameworkCore_CodeFirst.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.Property(x => x.Name)
+                 .IsRequired()
+                 .HasMaxLength(30)
+                 .HasDefaultValue("Default");
+    }
+}
---
+++ Lesson8_EntityFrameworkCore_CodeFirst/Program.cs
@@ -0,0 +1,43 @@
+﻿using Lesson8_EntityFrameworkCore_CodeFirst.Contexts;
+using Lesson8_EntityFrameworkCore_CodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+using ShopDbContext context = new ShopDbContext();
+
+
+
+
+//var list = context.Products .Where(x => x.Id < 10)
+//                            .Select(x => new
+//                                    {
+//                                        x.Id,
+//                                        x.Name,
+//                                        x.UnitPrice,
+//                                        x.UnitInStock
+//                                    })
+//                            .OrderBy(x => x.UnitPrice);
+
+
+
+// foreach (var p in list)
+// {
+//     Console.WriteLine($"{p.Id} {p.Name} {p.UnitPrice} {p.UnitInStock}");
+// }
+
+
+
+
+
+var p = context.Products.Find(1);
+Console.WriteLine($"{p.Id} {p.Name} {p.UnitPrice} {p.UnitInStock}");
+
+
+
+//context.Categories.IgnoreQueryFilters().Where(x => x.Id > 4);
+
+
+
+
+// Convertional
+// Data annotation
+// Fluent API
